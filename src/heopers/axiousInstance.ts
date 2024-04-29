d2c99fd27b58f7cc022685d5e5b191c32868fa05
arafat717 <arafatjibon33@gmail.com>
2024-04-29 08:26:02 +0600

    added axios instance response

--- src/heopers/axiousInstance.ts
+++ src/heopers/axiousInstance.ts
@@ -1,4 +1,9 @@
 import { authKey } from "@/contants/authkey";
+import {
+  IGenericErrorMessage,
+  IGenericErrorResponse,
+  ResponseSuccessType,
+} from "@/types";
 import { getFromLocalStorage } from "@/utils/loacl.storage";
 import axios from "axios";
 
@@ -27,15 +32,25 @@ instance.interceptors.request.use(
 
 // Add a response interceptor
 instance.interceptors.response.use(
+  //@ts-ignore
   function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
-    return response;
+    const responseObject: ResponseSuccessType = {
+      data: response?.data?.data,
+      meta: response?.data?.meta,
+    };
+    return responseObject;
   },
   function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
-    return Promise.reject(error);
+    const responseObject: IGenericErrorResponse = {
+      statusCode: error?.response?.data?.statusCode || 5000,
+      message: error?.response?.data?.message || "something went wrong",
+      errorMessages: error?.response?.data?.message,
+    };
+    return responseObject;
   }
 );
 
